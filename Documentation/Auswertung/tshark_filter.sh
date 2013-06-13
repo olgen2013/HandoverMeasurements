137782bf13242477b55fa089eb614966e91dd2c4
johannes <jg079@hdm-stuttgart.de>
2013-06-13 17:32:36 +0200

    added tshark_filter bash script - filters unidirectional communication (server -> client) - timestamp and packet size

---
+++ Documentation/Auswertung/tshark_filter.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` {input file}"
+  exit $E_BADARGS
+fi
+
+# biderectional communication between server and client
+#tshark -r $1 -T fields -e frame.time_relative -e frame.cap_len -R "ip.dst == 141.62.65.108 || ip.src == 141.62.65.108"> $1_filtered.csv
+
+# unidirectional communication (server -> client
+tshark -r $1 -T fields -e frame.time_relative -e frame.cap_len -R "ip.src == 141.62.65.108"> $1_filtered.csv
